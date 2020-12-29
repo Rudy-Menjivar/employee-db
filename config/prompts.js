451e78661b1329ea4f0f9f5120641d0de4f7b735
Rudy-Menjivar <rudy.menjivar@gmail.com>
2020-12-29 07:33:34 -0800

    Included menu prompts with inquirer

--- config/prompts.js
+++ config/prompts.js
@@ -4,6 +4,18 @@ var connection = require("./connection.js");
 mainMenu();
 
 function mainMenu() {
-    console.log("Under construction");
-
-}
+  inquirer.prompt({
+    name: "action",
+    type: "list",
+    message: "What would you like to do?",
+    choices: [
+        "View All Employees",
+        "View All Employees by Department",
+        "View All Employees by Manager",
+        "Add Employee",
+        "Remove Employee",
+        "Update Employee Role",
+        "Update Employee Manager"
+    ]
+  })
+};
