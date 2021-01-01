8758554990f5fa8b770a12cedc629bcac60f5fb8
Rudy-Menjivar <rudy.menjivar@gmail.com>
2021-01-01 08:02:38 -0800

    Replaced engTeam function with renderDept function that uses a placeholder value, so it can be used for similar functions

--- config/prompts.js
+++ config/prompts.js
@@ -65,7 +65,7 @@ function viewAllbyDept(){
     .then((answer) => { 
       switch (answer.deptName) {
         case "Engineering":
-          engTeam();
+          renderDept(answer);
           break;
         case "Finance":
           finTeam();
@@ -83,10 +83,13 @@ function viewAllbyDept(){
     })
 }
 
-function engTeam() {
-    query="SELECT CONCAT(employee.first_name, ' ', employee.last_name) AS 'Employee Name', role.title AS Title, department.name as Department, role.salary as Salary, CONCAT(mgr.first_name, ' ' ,mgr.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee mgr on employee.manager_id = mgr.id WHERE department.name = 'Engineering';"
-    renderResults();
-}
+function renderDept(answer) {
+    connection.query("SELECT CONCAT(employee.first_name, ' ', employee.last_name) AS 'Employee Name', role.title AS Title, department.name as Department, role.salary as Salary, CONCAT(mgr.first_name, ' ' ,mgr.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee mgr on employee.manager_id = mgr.id WHERE department.name = ?", [answer.deptName], function (err, res) {
+        if (err) throw err;
+        printTable(res)
+        mainMenu();
+    })
+};
 
 function finTeam() {
     query="SELECT CONCAT(employee.first_name, ' ', employee.last_name) AS 'Employee Name', role.title AS Title, department.name as Department, role.salary as Salary, CONCAT(mgr.first_name, ' ' ,mgr.last_name) AS Manager FROM employee INNER JOIN role on role.id = employee.role_id INNER JOIN department on department.id = role.department_id left join employee mgr on employee.manager_id = mgr.id WHERE department.name = 'Finance';"
