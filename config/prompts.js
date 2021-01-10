6f9430f1b2d19763baf23c9b5d1eeb9fd31c937a
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-01-09 19:51:28 -0800

    updated spacing for viewAllbyMgr function

--- config/prompts.js
+++ config/prompts.js
@@ -108,26 +108,26 @@ function mgtList() {
 };
 
 function viewAllbyMgr() {
-    inquirer.prompt({
-        name: "selection",
-        type: "list",
-        message: "View Employees by Manager:",
-        choices: empChoices
-    })
-    .then((answers) => {
-        var mgrId = getManagerID(answers.selection, empByMgr);
-        query = `SELECT id AS ID, CONCAT(first_name, ' ', last_name) AS EMPLOYEE_NAME FROM employee WHERE manager_id=${mgrId};`;
-        connection.query(query, function (err, res) {
-            if (err) throw err;
-            if (res.length === 0) {
-                console.log("")
-            }
-            else {
-                printTable(res);
-            }
-            mainMenu();
-        });
-    })
+  inquirer.prompt({
+    name: "selection",
+    type: "list",
+    message: "View Employees by Manager:",
+    choices: empChoices
+  })
+  .then((answers) => {
+      var mgrId = getManagerID(answers.selection, empByMgr);
+      query = `SELECT id AS ID, CONCAT(first_name, ' ', last_name) AS EMPLOYEE_NAME FROM employee WHERE manager_id=${mgrId};`;
+      connection.query(query, function (err, res) {
+        if (err) throw err;
+        if (res.length === 0) {
+            console.log("")
+        }
+        else {
+            printTable(res);
+        }
+        setTimeout(mainMenu, 2000);
+      });
+  })
 };
 
 function getManagerID(selection, mgrName) {
