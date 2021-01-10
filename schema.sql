809bb87a9efa1a7b1cfc79a058f696367ede0a0e
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-01-09 19:14:00 -0800

    changed varchar to upper case

--- schema.sql
+++ schema.sql
@@ -9,14 +9,14 @@ USE employee_db;
 -- Create the Department table
 CREATE TABLE department (
   id int NOT NULL AUTO_INCREMENT,
-  name varchar(30) NOT NULL,
+  name VARCHAR(30) NOT NULL,
   PRIMARY KEY (id)
 );
 
 -- Create the Role table
 CREATE TABLE role (
   id int NOT NULL AUTO_INCREMENT,
-  title varchar(30) NOT NULL,
+  title VARCHAR(30) NOT NULL,
   salary DECIMAL(10,2) NULL,
   department_id int NOT NULL,
   PRIMARY KEY (id),
@@ -26,8 +26,8 @@ CREATE TABLE role (
 -- Create the Employee table
 CREATE TABLE employee (
   id int NOT NULL AUTO_INCREMENT,
-  first_name varchar(30) NOT NULL,
-  last_name varchar(30) NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
   role_id int NOT NULL,
   manager_id int NULL,
   PRIMARY KEY (id),
