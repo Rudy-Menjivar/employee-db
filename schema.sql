a292059a097727ddc176561857e56ab4601e63f5
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-01-09 19:15:55 -0800

    removed extra lines that set primary key by moving pk attribute to id int

--- schema.sql
+++ schema.sql
@@ -8,29 +8,26 @@ USE employee_db;
 
 -- Create the Department table
 CREATE TABLE department (
-  id int NOT NULL AUTO_INCREMENT,
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id)
 );
 
 -- Create the Role table
 CREATE TABLE role (
-  id int NOT NULL AUTO_INCREMENT,
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(10,2) NULL,
   department_id int NOT NULL,
-  PRIMARY KEY (id),
   CONSTRAINT fk_dept_id FOREIGN KEY (department_id) REFERENCES department(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 -- Create the Employee table
 CREATE TABLE employee (
-  id int NOT NULL AUTO_INCREMENT,
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id int NOT NULL,
   manager_id int NULL,
-  PRIMARY KEY (id),
   CONSTRAINT fk_role_id FOREIGN KEY (role_id) REFERENCES role(id) ON UPDATE CASCADE ON DELETE CASCADE,
   CONSTRAINT fk_manager_id FOREIGN KEY (manager_id) REFERENCES employee(id) ON UPDATE CASCADE ON DELETE SET NULL
 );
